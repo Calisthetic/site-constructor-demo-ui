9fb23cdef0a19ee2f60941f829f08dde5df8fec9
Vladimir <72902666+Calisthetic@users.noreply.github.com>
2023-06-01 19:30:53 +0500

    ui changes

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,26 +1,44 @@
 import { useRef } from "react";
-import s from "./Pages.module.css"
+import s from "./Pages.module.css";
 
 const Auth = () => {
-  const textLoginRef = useRef()
-  const textPasswordRef = useRef()
+  const textLoginRef = useRef();
+  const textPasswordRef = useRef();
 
   async function Entry() {
-    if (textPasswordRef.current.value.length !== 0 && textLoginRef.current.value.length !== 0) {
+    if (
+      textPasswordRef.current.value.length !== 0 &&
+      textLoginRef.current.value.length !== 0
+    ) {
       window.location.href = "/projects";
     } else {
-      alert("Пользователь не найден")
+      alert("Пользователь не найден");
     }
   }
 
   return (
-    <div className={s.background}>
+    <div className={s.background_auth}>
       <div className={s.field_back}>
-        <input type="text" defaultValue="user1" ref={textLoginRef} placeholder="login or name" className={s.input}></input>
-        <input type="text" defaultValue="pass1" ref={textPasswordRef} placeholder="password" className={s.input}></input>
-        <div onClick={Entry} className={s.button}>Entry</div>
+        <div className={s.title}>Авторизация</div>
+        <input
+          type="text"
+          defaultValue="user1"
+          ref={textLoginRef}
+          placeholder="login or name"
+          className={s.input}
+        ></input>
+        <input
+          type="text"
+          defaultValue="pass1"
+          ref={textPasswordRef}
+          placeholder="password"
+          className={s.input}
+        ></input>
+        <div onClick={Entry} className={s.button}>
+          Entry
+        </div>
       </div>
     </div>
-  )
-}
-export default Auth
+  );
+};
+export default Auth;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,21 +1,31 @@
-import s from "./Pages.module.css"
+import s from "./Pages.module.css";
 
 const Projects = () => {
-
   function selectProject() {
     window.location.href = "/main";
   }
 
   return (
-    <div className={s.background}>
+    <div className={s.background_projects}>
       <div className={s.cages}>
-        <div className={s.cage} onClick={selectProject}>Project1</div>
-        <div className={s.cage} onClick={selectProject}>Project2</div>
-        <div className={s.cage} onClick={selectProject}>Project3</div>
-        <div className={s.cage} onClick={selectProject}>Project4</div>
-        <div className={s.cage} onClick={selectProject}>Project5</div>
+        <div className={s.titlecage}> ВАШИ ПРОЕКТЫ </div>
+        <div className={s.cage} onClick={selectProject}>
+          Project1
+        </div>
+        <div className={s.cage} onClick={selectProject}>
+          Project2
+        </div>
+        <div className={s.cage} onClick={selectProject}>
+          Project3
+        </div>
+        <div className={s.cage} onClick={selectProject}>
+          Project4
+        </div>
+        <div className={s.cage} onClick={selectProject}>
+          Project5
+        </div>
       </div>
     </div>
-  )   
-}
-export default Projects
+  );
+};
+export default Projects;
