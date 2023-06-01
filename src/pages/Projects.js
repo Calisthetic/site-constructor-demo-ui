0ff8fa52444866ae3c2bf36f01d7efe3ab79b21f
Vladimir <72902666+Calisthetic@users.noreply.github.com>
2023-06-01 21:43:04 +0500

    projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,26 +4,59 @@ const Projects = () => {
   function selectProject() {
     window.location.href = "/main";
   }
+  function addProject() {
+    document.getElementById("AddProjectField").style.width === "0px"
+      ? (document.getElementById("AddProjectField").style.width = "200px")
+      : (document.getElementById("AddProjectField").style.width = "0px");
+    document.getElementById("AddProjectField").style.marginLeft === "0px"
+      ? (document.getElementById("AddProjectField").style.marginLeft = "20px")
+      : (document.getElementById("AddProjectField").style.marginLeft = "0px");
+  }
 
   return (
     <div className={s.background_projects}>
-      <div className={s.cages}>
-        <div className={s.titlecage}> ВАШИ ПРОЕКТЫ </div>
-        <div className={s.cage} onClick={selectProject}>
-          Project1
+      <div className={s.projects_field}>
+        <div className={s.cages_back}>
+          <div title="Click to edit your project" className={s.titlecage}>
+            ВАШИ ПРОЕКТЫ
+          </div>
+          <div className={s.cages}>
+            <div className={s.cage_back}>
+              <div className={s.cage} onClick={selectProject}>
+                <div className={s.cage_name}>Projectiiisxcccvvvv</div>
+                <div className={s.cage_blocks_count}>22</div>
+              </div>
+            </div>
+            <div className={s.cage_back}>
+              <div className={s.cage} onClick={selectProject}>
+                <div className={s.cage_name}>Project2sxcccvvvvv</div>
+                <div className={s.cage_blocks_count}>122</div>
+              </div>
+            </div>
+            <div className={s.cage_back}>
+              <div className={s.cage} onClick={selectProject}>
+                <div className={s.cage_name}>Project3</div>
+                <div className={s.cage_blocks_count}>1</div>
+              </div>
+            </div>
+            <div className={s.cage_back}>
+              <div className={s.cage} onClick={selectProject}>
+                <div className={s.cage_name}>Project4</div>
+                <div className={s.cage_blocks_count}>14</div>
+              </div>
+            </div>
+            <div className={s.cage_back}>
+              <div className={s.cage} onClick={selectProject}>
+                <div className={s.cage_name}>Project5</div>
+                <div className={s.cage_blocks_count}>0</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={s.cage} onClick={selectProject}>
-          Project2
-        </div>
-        <div className={s.cage} onClick={selectProject}>
-          Project3
-        </div>
-        <div className={s.cage} onClick={selectProject}>
-          Project4
-        </div>
-        <div className={s.cage} onClick={selectProject}>
-          Project5
-        </div>
+        <div id="AddProjectField" className={s.add_project}></div>
+      </div>
+      <div className={s.add_button} onClick={addProject}>
+        +
       </div>
     </div>
   );
