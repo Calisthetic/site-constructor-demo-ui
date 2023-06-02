b3675672537bd049898231511ed225a8ca210271
Vladimir <72902666+Calisthetic@users.noreply.github.com>
2023-06-02 20:08:31 +0500

    Proj page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,25 +4,40 @@ import s from "./Projects.module.css";
 const Projects = () => {
   const [addOpen, setAddOpen] = useState(false);
 
-  const addProjectButton = useRef();
-  const addProjectField = useRef();
+  const addProjectButtonRef = useRef();
+  const addProjectOpenButtonRef = useRef();
+  const addProjectFieldRef = useRef();
+  const addProjectNameRef = useRef();
 
   function selectProject() {
     window.location.href = "/main";
   }
-  function addProject() {
+  function addProjectButtonClick() {
     if (!addOpen) {
-      addProjectField.current.style.width = "214px";
-      addProjectField.current.style.marginLeft = "30px";
-      addProjectButton.current.textContent = "−";
-      addProjectButton.current.style.transform = "rotate(180deg)";
+      addProjectFieldRef.current.style.width = "214px";
+      addProjectFieldRef.current.style.marginLeft = "30px";
+      addProjectOpenButtonRef.current.textContent = "−";
+      addProjectOpenButtonRef.current.style.transform = "rotate(180deg)";
+      addProjectOpenButtonRef.current.style.color = "#ff0000";
       setAddOpen(!addOpen);
     } else {
-      addProjectField.current.style.width = "0px";
-      addProjectField.current.style.marginLeft = "0px";
-      addProjectButton.current.textContent = "+";
-      addProjectButton.current.style.transform = "rotate(0deg)";
+      addProjectFieldRef.current.style.width = "0px";
+      addProjectFieldRef.current.style.marginLeft = "0px";
+      addProjectOpenButtonRef.current.textContent = "+";
+      addProjectOpenButtonRef.current.style.transform = "rotate(0deg)";
+      addProjectOpenButtonRef.current.style.color = "#00ff00";
       setAddOpen(!addOpen);
+    }
+  }
+  function addProject() {
+    if (addProjectNameRef.current.value.length > 2) {
+      addProjectNameRef.current.value = "";
+      addProjectButtonClick();
+    } else {
+      addProjectButtonRef.current.style.color = "#ff0000";
+      setTimeout(() => {
+        addProjectButtonRef.current.style.color = "lime";
+      }, 500);
     }
   }
 
@@ -66,21 +81,28 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        <div ref={addProjectField} className={s.add_project}>
+        <div ref={addProjectFieldRef} className={s.add_project}>
           <div className={s.add_title}>НОВЫЙ ПРОЕКТ</div>
           <input
             maxLength="32"
             placeholder="название проекта"
             type="text"
+            ref={addProjectNameRef}
             className={s.add_input_name}
           ></input>
-          <div className={s.add_button}>Создать</div>
+          <div
+            className={s.add_button}
+            ref={addProjectButtonRef}
+            onClick={addProject}
+          >
+            Создать
+          </div>
         </div>
       </div>
       <div
         className={s.add_open_button}
-        ref={addProjectButton}
-        onClick={addProject}
+        ref={addProjectOpenButtonRef}
+        onClick={addProjectButtonClick}
       >
         +
       </div>
